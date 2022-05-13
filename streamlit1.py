0ed77061ed2f14f29e4612e54c79a6e485cfdfaa
Anuraag Kulkarni <81404001+anuraag027@users.noreply.github.com>
2022-05-13 12:43:58 +0530

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -153,7 +153,8 @@ with st.sidebar:
                  'Attempted Dribbles_p90 (possession_p90)','Passes into Penalty Area_p90 (possession_p90)',
                  'Carries into Penalty Area_p90 (possession_p90)','npxG_p90 (shooting_p90)', 'npxG/Sh_p90 (shooting_p90)',
                  'Prog Actions_p90 (possession_p90)','Crs_p90 (passing_types_p90)','TB_p90 (passing_types_p90)',
-                 'Prog Carries_p90 per 100 touches (possession_p90)', 'Sh_p90 (possession_p90)', 'SoT_p90 (possession_p90)']]
+                 'Prog Carries_p90 per 100 touches (possession_p90)', 'Sh_p90 (possession_p90)', 'SoT_p90 (possession_p90)', 
+                 'Receiving Prog_p90 (possession_p90)']]
 
     elif pos == 'Defensive Midfield':
         df = df[['Player','Squad','Age','Successful Dribbles_p90 (possession_p90)',
@@ -170,7 +171,8 @@ with st.sidebar:
                  'Passes into Final 1/3_p90 (possession_p90)','Prog Carries_p90 (possession_p90)',
                  'Prog Passes_p90 (possession_p90)','Carries into Final 1/3_p90 (possession_p90)',
                  'Passes into Penalty Area_p90 (possession_p90)','Carries into Penalty Area_p90 (possession_p90)',
-                 'xA_p90 (passing_p90)','KP_p90 (possession_p90)','npxG_p90 (shooting_p90)','TB_p90 (passing_types_p90)']]
+                 'xA_p90 (passing_p90)','KP_p90 (possession_p90)','npxG_p90 (shooting_p90)','TB_p90 (passing_types_p90)', 
+                 'Receiving Prog_p90 (possession_p90)']]
 
     elif pos == 'Central Midfield':
         df = df[['Player','Squad','Age','Successful Dribbles_p90 (possession_p90)',
@@ -183,7 +185,8 @@ with st.sidebar:
     elif pos == 'Centre-Forward':
         df = df[['Player','Squad','Age','xA_p90 (passing_p90)','Passes into Penalty Area_p90 (possession_p90)',
                  'Carries into Penalty Area_p90 (possession_p90)','npxG_p90 (shooting_p90)', 'npxG/Sh_p90 (shooting_p90)',
-                 'Sh_p90 (possession_p90)', 'SoT_p90 (possession_p90)','Aerial Win % (possession_p90)','Won_p90 (misc_p90)']]
+                 'Sh_p90 (possession_p90)', 'SoT_p90 (possession_p90)','Aerial Win % (possession_p90)','Won_p90 (misc_p90)',
+                 'Receiving Prog_p90 (possession_p90)']]
 
     elif pos == 'Goalkeeper':
         df = df[['Player','Squad','Age','Completed Passes_p90 (passing_p90)','SoTA_p90 (keepers_p90)', 'Saves_p90 (keepers_p90)','Stp_p90 (keepersadv_p90)',
@@ -194,17 +197,17 @@ with st.sidebar:
         df = df[['Player','Squad','Age','Successful Dribbles_p90 (possession_p90)', 'Sh_p90 (possession_p90)', 'SoT_p90 (possession_p90)',
                  'Passes into Penalty Area_p90 (possession_p90)','Carries into Penalty Area_p90 (possession_p90)',
                  'xA_p90 (passing_p90)','KP_p90 (possession_p90)','npxG_p90 (shooting_p90)',
-                 'Aerial Win % (possession_p90)','Won_p90 (misc_p90)']]
+                 'Aerial Win % (possession_p90)','Won_p90 (misc_p90)', 'Receiving Prog_p90 (possession_p90)']]
 
     elif pos == 'Centre-Back':
         df = df[['Player','Squad','Age',
                  'Padj Tkl+Int p90 (defense_Padj_p90)','% of Dribblers Tackled (possession_p90)',
                  'Completed Passes_p90 (passing_p90)','Long Cmp_p90 (passing_p90)','Long Att_p90 (passing_p90)',
+                 'Prog Carries_p90 per 100 touches (possession_p90)',
                  'Prog Passes_p90 per 50 passes (possession_p90)','Passes into Final 1/3_p90 (possession_p90)',
                  'Prog Carries_p90 (possession_p90)','Prog Passes_p90 (possession_p90)','Clr_p90 (defense_Padj_p90)',
-                 'Sh_p90 (defense_Padj_p90)', 'Pass_p90 (defense_Padj_p90)','Prog Carries_p90 per 100 touches (possession_p90)',
+                 'Sh_p90 (defense_Padj_p90)', 'Pass_p90 (defense_Padj_p90)',
                  'Aerial Win % (possession_p90)','Won_p90 (misc_p90)','True Interceptions_p90 (possession_p90)']]
-
     df = df.dropna()
     #Reset index
     df.reset_index(inplace=True)
