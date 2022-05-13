c546871b697fe66e5c1d804881a1cff351148d57
Anuraag Kulkarni <81404001+anuraag027@users.noreply.github.com>
2022-05-13 19:44:41 +0530

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -26,12 +26,20 @@ import math
 import warnings
 warnings.filterwarnings('ignore')
 
+# ADDING CACHE OPTIONS
+@st.cache
+def load_data():
+    return pd.read_csv('Data/All_stats_combined_with_positions.csv')
+df = load_data()
+
+@st.cache
+
 st.header('Use Machine Learning to Find Similar Players')
 st.caption('Select a player, and my Machine Learning model will suggest players similar to the selected player.')
 st.caption('Made by: Anuraag Kulkarni | Twitter: @Anuraag027')
 
-[theme]
-base="light"
+# [theme]
+# base="light"
 
 def plot_radar(bkup,df,n):
 #     print("n:",n)
