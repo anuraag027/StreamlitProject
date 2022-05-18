d3cda68e148fa6cea55572560fd30308c37c3eed
Anuraag Kulkarni <81404001+anuraag027@users.noreply.github.com>
2022-05-18 23:15:13 +0530

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -195,10 +195,9 @@ with st.sidebar:
     if (pos == 'Right-Back') or (pos == 'Left-Back') or (pos == 'Left WingBack') or (pos == 'Right WingBack'):
         df = df[['Player','Squad','Age','Padj Tkl+Int p90 (defense_Padj_p90)','% of Dribblers Tackled (possession_p90)','Crs_p90 (passing_types_p90)',
                  'SCA_p90 (gca_p90)',
-                 'xA_p90 (passing_p90)','Passes into Final 1/3_p90 (possession_p90)', 'Receiving Prog_p90 (possession_p90)',
+                 'xA_p90 (passing_p90)','Prog Actions_p90 (possession_p90)','Passes into Final 1/3_p90 (possession_p90)',
                  'Carries into Final 1/3_p90 (possession_p90)','KP_p90 (possession_p90)','TB_p90 (passing_types_p90)',
-                 'True Interceptions_p90 (possession_p90)','Prog Carries_p90 per 100 touches (possession_p90)',
-                 'Prog Carries_p90 (possession_p90)','Prog Passes_p90 (possession_p90)']]
+                 'True Interceptions_p90 (possession_p90)','Prog Carries_p90 per 100 touches (possession_p90)']]
 
     elif (pos == 'Right Winger') or (pos == 'Left Winger'):
         df = df[['Player','Squad','Age','xA_p90 (passing_p90)','KP_p90 (possession_p90)','Successful Dribbles_p90 (possession_p90)',
@@ -207,7 +206,7 @@ with st.sidebar:
                  'Prog Actions_p90 (possession_p90)','Crs_p90 (passing_types_p90)','TB_p90 (passing_types_p90)',
                  'Prog Carries_p90 per 100 touches (possession_p90)', 'Sh_p90 (possession_p90)', 'SoT_p90 (possession_p90)', 
                  'Receiving Prog_p90 (possession_p90)','Prog Carries_p90 (possession_p90)','Prog Passes_p90 (possession_p90)',
-                 'Prog Passes_p90 per 50 passes (possession_p90)']]
+                 'Prog Passes_p90 per 50 passes (possession_p90)','Off_p90 (misc_p90)']]
 
     elif pos == 'Defensive Midfield':
         df = df[['Player','Squad','Age','Successful Dribbles_p90 (possession_p90)',
@@ -240,7 +239,8 @@ with st.sidebar:
         df = df[['Player','Squad','Age','xA_p90 (passing_p90)','Passes into Penalty Area_p90 (possession_p90)',
                  'Carries into Penalty Area_p90 (possession_p90)','npxG_p90 (shooting_p90)', 'npxG/Shot (possession_p90)',
                  'Sh_p90 (possession_p90)', 'SoT_p90 (possession_p90)','Aerial Win % (possession_p90)','Won_p90 (misc_p90)',
-                 'Receiving Prog_p90 (possession_p90)','SCA_p90 (gca_p90)','KP_p90 (possession_p90)','Off_p90 (misc_p90)']]
+                 'Receiving Prog_p90 (possession_p90)','SCA_p90 (gca_p90)','KP_p90 (possession_p90)','Off_p90 (misc_p90)',
+                 'Average Shot Distance (yards) (possession_p90)']]
 
     elif pos == 'Goalkeeper':
         df = df[['Player','Squad','Age','Completed Passes_p90 (passing_p90)','SoTA_p90 (keepers_p90)', 'Saves_p90 (keepers_p90)','Stp_p90 (keepersadv_p90)',
@@ -252,7 +252,7 @@ with st.sidebar:
                  'Passes into Penalty Area_p90 (possession_p90)','Carries into Penalty Area_p90 (possession_p90)',
                  'xA_p90 (passing_p90)','KP_p90 (possession_p90)','npxG_p90 (shooting_p90)','SCA_p90 (gca_p90)',
                  'Aerial Win % (possession_p90)','Won_p90 (misc_p90)', 'Receiving Prog_p90 (possession_p90)',
-                 'npxG/Shot (possession_p90)','Off_p90 (misc_p90)']]
+                 'npxG/Shot (possession_p90)','Off_p90 (misc_p90)','Average Shot Distance (yards) (possession_p90)']]
 
     elif pos == 'Centre-Back':
         df = df[['Player','Squad','Age',
@@ -262,7 +262,8 @@ with st.sidebar:
                  'Prog Passes_p90 per 50 passes (possession_p90)','Passes into Final 1/3_p90 (possession_p90)',
                  'Prog Carries_p90 (possession_p90)','Prog Passes_p90 (possession_p90)','Clr_p90 (defense_Padj_p90)',
                  'Shots Blocked_p90 (possession_p90)', 'Pass_p90 (defense_Padj_p90)',
-                 'Aerial Win % (possession_p90)','Won_p90 (misc_p90)','True Interceptions_p90 (possession_p90)']]        
+                 'Aerial Win % (possession_p90)','Won_p90 (misc_p90)','True Interceptions_p90 (possession_p90)']]
+ 
    
     df = df.dropna()
     #Reset index
@@ -391,7 +392,8 @@ test_dic = {
 'Sh_p90 (possession_p90)' : 'Shots Taken per 90',
 'npxG/Shot (possession_p90)' : 'Non Penalty xG Generated per Shot',
 'Clr_p90 (defense_Padj_p90)' : 'Clearances Made per 90',
-'Off_p90 (misc_p90)' : 'Offsides per 90'}
+'Off_p90 (misc_p90)' : 'Offsides per 90',
+'Average Shot Distance (yards) (possession_p90)' : 'Average Distance of Shots Taken (in yards)'}
 
 st.caption('The lower the similarity score, the higher the similarity between the players. A similarity score of above 4.5 should be taken with a pinch of salt.')
     
