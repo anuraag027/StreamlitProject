7e928786ffa82f7e244f933ea17b738e7268dd48
Anuraag Kulkarni <81404001+anuraag027@users.noreply.github.com>
2023-06-07 19:02:20 -0400

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -253,7 +253,7 @@ with st.sidebar:
     elif pos == 'Centre-Forward':
         df = df[['Player','Squad','Age','xA per KP p90 (passing_p90)','Passes into Penalty Area_p90 (passing_p90)',
                  'Carries into Penalty Area_p90 (possession_p90)','npxG_p90 (shooting_p90)', 'npxG/Sh_p90 (shooting_p90)',
-                 'Sh_p90 (shooting_p90)', 'SoT_p90 (shooting_p90)','Aerial Win % (possession_p90)','Won_p90 (misc_p90)',
+                 'Sh_p90 (shooting_p90)', 'SoT_p90 (shooting_p90)','Aerial Win % (misc_p90)','Won_p90 (misc_p90)',
                  'Prog Passes Received_p90 (stats_p90)','SCA_p90 (gca_p90)','KP p90 per 50 passes (passing_p90)','Off_p90 (misc_p90)',
                  'Average Shot Distance (yards) (possession_p90)', 'xAG_p90 (passing_p90)', 'xA_p90 (passing_p90)',
                  'Prog Passes_p90 (passing_p90)']]
@@ -268,7 +268,7 @@ with st.sidebar:
         df = df[['Player','Squad','Age','Successful Dribbles_p90 (possession_p90)', 'Sh_p90 (possession_p90)', 'SoT_p90 (shooting_p90)',
                  'Passes into Penalty Area_p90 (passing_p90)','Carries into Penalty Area_p90 (possession_p90)',
                  'xA per KP p90 (passing_p90)','KP p90 per 50 passes (passing_p90)','npxG_p90 (shooting_p90)','SCA_p90 (gca_p90)',
-                 'Aerial Win % (possession_p90)','Won_p90 (misc_p90)', 'Prog Passes Received_p90 (stats_p90)',
+                 'Aerial Win % (misc_p90)','Won_p90 (misc_p90)', 'Prog Passes Received_p90 (stats_p90)',
                  'npxG/Sh_p90 (shooting_p90)','Off_p90 (misc_p90)','Average Shot Distance (yards) (possession_p90)']]
 
     elif pos == 'Centre-Back':
@@ -279,7 +279,7 @@ with st.sidebar:
                  'Prog Passes_p90 per 50 passes (possession_p90)','Passes into Final 1/3_p90 (passing_p90)',
                  'Prog Carries_p90 (possession_p90)','Prog Passes_p90 (passing_p90)','Clr_p90 (defense_Padj_p90)',
                  'Shots Blocked_p90 (possession_p90)', 'Pass_p90 (defense_Padj_p90)',
-                 'Aerial Win % (possession_p90)','Won_p90 (misc_p90)','True Interceptions_p90 (defense_p90)']]
+                 'Aerial Win % (misc_p90)','Won_p90 (misc_p90)','True Interceptions_p90 (defense_p90)']]
 
     #Reset index
     df.reset_index(inplace=True)
