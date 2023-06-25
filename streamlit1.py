601581b81de61e75a47f8d8e01bce5d7e4d3521f
Anuraag Kulkarni <81404001+anuraag027@users.noreply.github.com>
2023-06-25 15:29:49 -0400

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -20,10 +20,7 @@ def load_data(): #Function to load csv data into dataframe
     return df,gk_df
 df,gk_df = load_data()
 @st.cache(allow_output_mutation=True)
-
 st.write(df[df['Player'] == 'Rodri'])
-
-
 
 def create_scaler_model(): #Function to create standard scaler model
     return StandardScaler()
