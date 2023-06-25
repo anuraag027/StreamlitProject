2dc25ccef5d4034953598a379752afc95cad96d9
Anuraag Kulkarni <81404001+anuraag027@users.noreply.github.com>
2023-06-25 15:11:25 -0400

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -449,8 +449,8 @@ for ele in try_list:
                 st.write(str(cnt)+'.',ele,' : ',v)
 
 #Acknowledgements
-st.caption('Names of players are as taken as provided by Statsbomb on FBRef.com')
-st.caption('Data has been taken from FBRef (Statsbomb): https://fbref.com/en/')
+st.caption('Names of players are as taken as provided by Opta on FBRef.com')
+st.caption('Data has been taken from FBRef (Opta): https://fbref.com/en/')
 st.caption('Player positions as defined by Jase: https://twitter.com/jaseziv/status/1520148594866913280?s=20&t=gczD9hyTCCHCU81w8-EBYw')
 st.caption('Feel free to reach out to me on Twitter via DMs if you have any questions or if you face any issues with the app.')
 st.caption('This app only takes publicly available metrics to find similarities. This will not be 100% accurate so please DO NOT use the results as conclusive evidence of player profiles.')
