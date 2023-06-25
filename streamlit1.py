aa3a8bbe0b0e2423fbb8363816d044593bb80bd2
Anuraag Kulkarni <81404001+anuraag027@users.noreply.github.com>
2023-06-25 16:11:09 -0400

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -219,7 +219,7 @@ with st.sidebar:
     elif pos == 'Attacking Midfield':
         df = df[['Player','Squad','Age','Successful Dribbles_p90 (possession_p90)', 'Sh_p90 (shooting_p90)',
                  'Completed Passes_p90 (passing_p90)','Prog Carries_p90 per 100 touches (possession_p90)', 'Def 3rd_p90 (possession_p90)', 'Mid 3rd_p90 (possession_p90)', 'Att 3rd_p90 (possession_p90)','Live_p90 (possession_p90)',
-                 'Passes into Final 1/3_p90 (passing_p90)','Prog Carries_p90 (possession_p90)','Passes into Final 1/3_p90 (passing_p90)',
+                 'Passes into Final 1/3_p90 (passing_p90)','Prog Carries_p90 (possession_p90)',
                  'Prog Passes_p90 (passing_p90)','Prog Passes_p90 per 50 passes (passing_p90)','Carries into Final 1/3_p90 (possession_p90)',
                  'Passes into Penalty Area_p90 (passing_p90)','Carries into Penalty Area_p90 (possession_p90)', 'Att Pen_p90 (possession_p90)',
                  'xA per KP p90 (passing_p90)','KP p90 per 50 passes (passing_p90)','npxG_p90 (shooting_p90)','TB_p90 (passing_types_p90)', 
